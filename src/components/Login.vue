982ab3e0323d80df4908abd9a5f4ac9c82917a2d
hector <sheep123_yang@163.com>
2021-05-01 16:31:07 +0800

    表单登录之前的预验证

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -19,7 +19,7 @@
         </el-form-item>
         <!--按钮区域-->
         <el-form-item class="btns">
-          <el-button type="primary" >登录</el-button>
+          <el-button type="primary" @click="login">登录</el-button>
           <el-button type="info" v-on:click="resetLoginForm">重置</el-button>
         </el-form-item>
 
@@ -57,6 +57,11 @@ export default {
     resetLoginForm () {
       console.log(this)
       this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      this.$refs.loginFormRef.validate(valid => {
+        console.log(valid)
+      })
     }
   }
 }
