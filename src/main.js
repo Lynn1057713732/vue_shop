ce5ac41b7fb72fb5766b3de9cd0a016d5732e261
hector <sheep123_yang@163.com>
2021-05-02 21:30:13 +0800

    axios请求拦截器添加token

--- src/main.js
+++ src/main.js
@@ -11,6 +11,13 @@ import axios from 'axios'
 
 // 设置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  // console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // console.log(config)
+  // 在最后必须 return config
+  return config
+})
 // 挂载原型属性
 Vue.prototype.$http = axios
 
