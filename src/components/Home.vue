91c0a256d2c581a154a2e3f985b153f3982ab0e4
hector <sheep123_yang@163.com>
2021-05-02 21:58:56 +0800

    侧边栏的折叠和展开效果

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,9 +11,13 @@
     <!-- 页面主体区域-->
     <el-container>
       <!--  侧边栏-->
-      <el-aside width="200px">
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
         <!-- 侧边栏菜单区域 -->
-        <el-menu background-color="#333744" text-color="#fff" active-text-color="#409EFF" unique-opened :collapse="isCollapse">
+        <el-menu background-color="#333744" text-color="#fff"
+                 active-text-color="#409EFF" unique-opened :collapse="isCollapse"
+                 :collapse-transition="false"
+        >
           <!-- 一级菜单 -->
           <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
             <!--              一级菜单的模板区-->
@@ -77,6 +81,9 @@ export default {
       if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
       this.menulist = res.data
       console.log(res)
+    },
+    toggleCollapse () {
+      this.isCollapse = !this.isCollapse
     }
   }
 }
@@ -116,5 +123,15 @@ export default {
 
 .el-main {
   background-color: #eaedf1;
+}
+
+.toggle-button {
+  background-color: #4a5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
 }
 </style>
