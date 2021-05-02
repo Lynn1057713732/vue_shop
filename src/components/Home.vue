18a400d64f4b7cd789ca9b6c7836310d4d0c0aaf
hector <sheep123_yang@163.com>
2021-05-02 21:52:51 +0800

    侧边栏的优化：图标，单一展示一级菜单，边框问题

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -13,25 +13,24 @@
       <!--  侧边栏-->
       <el-aside width="200px">
         <!-- 侧边栏菜单区域 -->
-        <el-menu background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
-
-          <!--            一级菜单-->
-          <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id" >
+        <el-menu background-color="#333744" text-color="#fff" active-text-color="#409EFF" unique-opened :collapse="isCollapse">
+          <!-- 一级菜单 -->
+          <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
             <!--              一级菜单的模板区-->
             <template slot="title">
               <!--              一级菜单的图标-->
-              <i class="el-icon-location"></i>
+              <i :class="iconsObj[item.id]"></i>
               <!--              一级菜单的文本-->
-              <span>{{item.authName}}</span>
+              <span>{{ item.authName }}</span>
             </template>
             <!--二级菜单-->
             <el-submenu :index="subItem.id" v-for="subItem in item.children" :key="subItem.id">
               <!--  二级菜单的模板区-->
               <template slot="title">
                 <!--  二级菜单的图标-->
-                <i class="el-icon-location"></i>
+                <i class="el-icon-menu"></i>
                 <!--  二级菜单的文本-->
-                <span>{{subItem.authName}}</span>
+                <span>{{ subItem.authName }}</span>
               </template>
             </el-submenu>
           </el-submenu>
@@ -49,7 +48,18 @@ export default {
   data () {
     return {
       // 左侧菜单数据
-      menulist: []
+      menulist: [],
+      iconsObj: {
+        125: 'iconfont icon-user',
+        103: 'iconfont icon-tijikongjian',
+        101: 'iconfont icon-shangpin',
+        102: 'iconfont icon-danju',
+        145: 'iconfont icon-baobiao'
+      },
+      // 是否折叠
+      isCollapse: false,
+      // 被激活的链接地址
+      activePath: ''
     }
   },
   created () {
@@ -98,6 +108,10 @@ export default {
 
 .el-aside {
   background-color: #333744;
+}
+
+.el-aside .el-menu {
+  border-right: none;
 }
 
 .el-main {
