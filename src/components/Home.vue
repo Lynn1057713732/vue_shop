315ab1201d0889cc351909eb2405ee4da6ffcb9e
hector <sheep123_yang@163.com>
2021-05-02 22:27:47 +0800

    侧边栏的重定向和链接的改造

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,34 +14,33 @@
       <el-aside :width="isCollapse ? '64px' : '200px'">
         <div class="toggle-button" @click="toggleCollapse">|||</div>
         <!-- 侧边栏菜单区域 -->
-        <el-menu background-color="#333744" text-color="#fff"
-                 active-text-color="#409EFF" unique-opened :collapse="isCollapse"
-                 :collapse-transition="false"
-        >
+        <el-menu background-color="#333744" text-color="#fff" active-text-color="#409EFF" unique-opened :collapse="isCollapse" :collapse-transition="false" router :default-active="activePath">
           <!-- 一级菜单 -->
           <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
             <!--              一级菜单的模板区-->
             <template slot="title">
-              <!--              一级菜单的图标-->
+              <!-- 图标 -->
               <i :class="iconsObj[item.id]"></i>
-              <!--              一级菜单的文本-->
-              <span>{{ item.authName }}</span>
+              <!-- 文本 -->
+              <span>{{item.authName}}</span>
             </template>
             <!--二级菜单-->
-            <el-submenu :index="subItem.id" v-for="subItem in item.children" :key="subItem.id">
-              <!--  二级菜单的模板区-->
+            <el-menu-item :index="'/' + subItem.path" v-for="subItem in item.children" :key="subItem.id" @click="saveNavState('/' + subItem.path)">
               <template slot="title">
-                <!--  二级菜单的图标-->
+                <!-- 图标 -->
                 <i class="el-icon-menu"></i>
-                <!--  二级菜单的文本-->
-                <span>{{ subItem.authName }}</span>
+                <!-- 文本 -->
+                <span>{{subItem.authName}}</span>
               </template>
-            </el-submenu>
+            </el-menu-item>
           </el-submenu>
         </el-menu>
       </el-aside>
       <!-- 右侧内容主体-->
-      <el-main>Main</el-main>
+      <el-main>
+        <!-- 路由占位符-->
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
@@ -123,6 +122,10 @@ export default {
 
 .el-main {
   background-color: #eaedf1;
+}
+
+.iconfont {
+  margin-right: 10px;
 }
 
 .toggle-button {
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome'
+import Users from '../components/user/Users'
 
 Vue.use(Router)
 
@@ -11,12 +13,12 @@ const router = new Router({
     { path: '/login', component: Login },
     {
       path: '/home',
-      component: Home
-      // redirect: '/welcome',
-      // children: [
-      //   { path: '/welcome', component: Welcome },
-      //   { path: '/users', component: Users }
-      // ]
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        { path: '/welcome', component: Welcome },
+        { path: '/users', component: Users }
+      ]
     }
   ]
 })
