9fec8ad027bb14759c7543bee1c72fe091f591b2
hector <sheep123_yang@163.com>
2021-05-05 10:24:13 +0800

    使用nprogress美化网页加载的进度条

--- src/main.js
+++ src/main.js
@@ -6,8 +6,6 @@ import './plugins/element.js'
 import './assets/fonts/iconfont.css'
 // 导入全局样式表
 import './assets/css/global.css'
-// 全局配置axios
-import axios from 'axios'
 
 import TreeTable from 'vue-table-with-tree-grid'
 // 导入富文本编辑器
@@ -17,23 +15,34 @@ import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
 
-Vue.component('tree-table', TreeTable)
-// 将富文本编辑器，注册为全局可用的组件
-Vue.use(VueQuillEditor)
+// 导入 NProgress 包对应的JS和CSS
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+// 全局配置axios
+import axios from 'axios'
 
-// 设置请求的根路径
+// 配置请求的跟路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 在 request 拦截器中，展示进度条 NProgress.start()
 axios.interceptors.request.use(config => {
   // console.log(config)
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
-  // console.log(config)
   // 在最后必须 return config
   return config
 })
-// 挂载原型属性
+// 在 response 拦截器中，隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
+// 将富文本编辑器，注册为全局可用的组件
+Vue.use(VueQuillEditor)
 
 Vue.filter('dateFormat', function (originVal) {
   const dt = new Date(originVal)
